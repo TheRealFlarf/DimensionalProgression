e78d4caea36d03b3e1db81facbc9b257fac71697
TheRealFlarf <flarf@DESKTOP-D0MEQQ6.fios-router.home>
2023-12-09 12:55:25 -0500

    Added a few loot table changes, and (hopefully) fixed the biome issue for S1lv3r

--- src/main/java/net/therealflarf/dimprogress/block/CrimsonCrimsoilGrassBlock.java
+++ src/main/java/net/therealflarf/dimprogress/block/CrimsonCrimsoilGrassBlock.java
@@ -1,17 +1,12 @@
 
 package net.therealflarf.dimprogress.block;
 
-import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.core.BlockPos;
-
-import java.util.List;
-import java.util.Collections;
 
 public class CrimsonCrimsoilGrassBlock extends Block {
 	public CrimsonCrimsoilGrassBlock() {
@@ -21,13 +16,5 @@ public class CrimsonCrimsoilGrassBlock extends Block {
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
 		return 15;
-	}
-
-	@Override
-	public List<ItemStack> getDrops(BlockState state, LootParams.Builder builder) {
-		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
-		if (!dropsOriginal.isEmpty())
-			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
 	}
 }
